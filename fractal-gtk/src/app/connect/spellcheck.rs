1ebd17c23558c82373489d1d0353cc9bddd57735
Eisha Chen-yen-su <chenyensu0@gmail.com>
2018-07-20 11:17:30 +0200

    spellcheck.rs: Change to what it could be like
    This changes `connect_spellcheck` to what it could be like if there was
    the required bindings. Leave commented what is not already working.
    
    See https://gitlab.gnome.org/World/fractal/issues/154

--- fractal-gtk/src/app/connect/spellcheck.rs
+++ fractal-gtk/src/app/connect/spellcheck.rs
@@ -1,18 +1,23 @@
 extern crate gtk;
+extern crate sourceview;
 
 use gspell;
-use gspell::EntryExt;
+// use gspell::TextViewExt;
+use self::gtk::prelude::*;
+
 use app::App;
 
 impl App {
     pub fn connect_spellcheck(&self) {
-        let msg_entry: gtk::Entry = self.ui.builder
+        let msg_entry: sourceview::View = self.ui.builder
             .get_object("msg_entry")
             .expect("Couldn't find msg_entry in ui file.");
 
-        /* Add gspell to the send Entry and enable the basic configuration */
-        if let Some(gspell_entry) = gspell::Entry::get_from_gtk_entry(&msg_entry) {
-            gspell::Entry::basic_setup(&gspell_entry);
-        }
+        let msg_entry: gtk::TextView = msg_entry.clone().upcast();
+
+        /* Add gspell to the send TextView and enable the basic configuration */
+        // if let Some(gspell_text_view) = gspell:TextView::get_from_gtk_text_view(&msg_entry) {
+        //     gspell::TextView::basic_set_up(&gspell_entry);
+        // }
     }
 }
