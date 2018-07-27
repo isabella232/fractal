66f0bd343dd3fa3d1f280513b44434d930feebbd
Eisha Chen-yen-su <chenyensu0@gmail.com>
2018-07-27 12:45:46 +0200

    UI: Add a dialog to show messages' source
    See https://gitlab.gnome.org/World/fractal/issues/195

--- fractal-gtk/src/uibuilder.rs
+++ fractal-gtk/src/uibuilder.rs
@@ -60,6 +60,8 @@ impl UI {
                .expect("Can't load ui file: account_settings.ui");
         builder.add_from_resource("/org/gnome/Fractal/ui/media_viewer.ui")
                .expect("Can't load ui file: media_viewer.ui");
+       builder.add_from_resource("/org/gnome/Fractal/ui/source_dialog.ui")
+              .expect("Can't load ui file: source_dialog.ui");
 
         // Depends on room config
         /*
