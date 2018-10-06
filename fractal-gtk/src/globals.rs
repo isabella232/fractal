0a4a465684c8e994d1c03384e17c0813b62a86f9
Eisha Chen-yen-su <chenyensu0@gmail.com>
2018-10-06 13:14:53 +0200

    Member: Add a widget for member pills
    See https://gitlab.gnome.org/World/fractal/issues/78

--- fractal-gtk/src/globals.rs
+++ fractal-gtk/src/globals.rs
@@ -2,6 +2,7 @@ pub static INITIAL_MESSAGES: usize = 40;
 pub static CACHE_SIZE: usize = 40;
 pub static MSG_ICON_SIZE: i32 = 40;
 pub static USERLIST_ICON_SIZE: i32 = 30;
+pub static PILL_ICON_SIZE: i32 = 18;
 pub static MINUTES_TO_SPLIT_MSGS: i64 = 30;
 pub static DEFAULT_HOMESERVER: &'static str = "https://matrix.org";
 pub static DEFAULT_IDENTITYSERVER: &'static str = "https://vector.im";
