919037495f7954267bae922174971ddef46d4e8c
Christopher Davis <brainblasted@disroot.org>
2019-04-22 01:54:26 -0400

    build: Place target in meson build dir
    Instead of putting target/ and in the source
    directory, we can tell our cargo script to put both target
    and our cargo-home in meson's build directory. This means
    that when triggering a rebuild through meson via tooling
    like Builder, Fractal will actually rebuild.

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-export CARGO_HOME=$1/target/cargo-home
-export FRACTAL_PROFILE="$3"
+export OUTPUT="$2"
+export CARGO_TARGET_DIR="$3"/target
+export CARGO_HOME="$CARGO_TARGET_DIR"/cargo-home
+export FRACTAL_PROFILE="$4"
 
 if [ "$FRACTAL_PROFILE" = "Devel" ]
 then
     echo "DEBUG MODE"
-    cargo build --manifest-path $1/Cargo.toml -p fractal-gtk && cp $1/target/debug/fractal-gtk $2
+    cargo build --manifest-path $1/Cargo.toml -p fractal-gtk && cp "$CARGO_TARGET_DIR"/debug/fractal-gtk "$OUTPUT"
 else
     echo "RELEASE MODE"
-    cargo build --manifest-path $1/Cargo.toml --release -p fractal-gtk && cp $1/target/release/fractal-gtk $2
+    cargo build --manifest-path $1/Cargo.toml --release -p fractal-gtk && cp "$CARGO_TARGET_DIR"/release/fractal-gtk "$OUTPUT"
 fi
